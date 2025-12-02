ecdc6afb4e8a832ad5d90f1af0516c95319f348c
AliSamerShaboot19 <www.ali.s.samer@gmail.com>
2025-12-02 10:21:46 +0300

    create server and connect to mongodb

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("mongodb connected");
+  })
+  .catch((error) => {
+    console.log("Error ", error);
+  });
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
